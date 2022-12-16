b7c35042794a15d91f9d2c83b8507e0f0887ac4b
Juan Bautista Muñoz Ruiz <jbmr0001@red.ujaen.es>
2022-12-16 16:35:44 +0100

    redondeo de valocidades para cvs

--- SimulacionMoto/src/simulacionmoto/Circuito.java
+++ SimulacionMoto/src/simulacionmoto/Circuito.java
@@ -123,6 +123,8 @@ public class Circuito {
                 }
                 
             }
+
+            vmaxPendienteAplicada=(double)Math.round(vmaxPendienteAplicada);
             this.velocidadMaximaCalculada.add(vmaxPendienteAplicada);
             this.velocidadMinimaCalculada.add(vmaxPendienteAplicada*Double.valueOf("0.5"));
 
